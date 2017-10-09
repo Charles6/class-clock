3cf767a1ad199a3aa30bd1bae9864ebdd1de53fc
Charles Nguyen <cpnguy@gmail.com>
2017-10-08 17:54:24 -0700

    agenda and UI
    Improved UI and added agenda system

---
+++ agenda.js
@@ -0,0 +1,21 @@
+var agendadbRef = firebase.database().ref().child("agendaItems");
+var agendaStack = [];
+
+agendadbRef.on('child_added', snap => {
+  var agendaObj = snap.val();
+
+  agendaStack.push(agendaObj.agenda);
+  var agendaNode = document.createElement("LI");
+  agendaNode.className = "agendaLI";
+  agendaNode.innerHTML = "<input type='checkbox'><label>" + agendaObj.agenda + "</label>";
+  document.getElementById("agendaContainer").appendChild(agendaNode);
+});
+
+
+const newAgenda = () => {
+  var inputText = document.getElementById("agendaInput");
+  firebase.database().ref("agendaItems").push({
+    agenda: inputText.value
+  });
+  document.getElementById('agendaInput').value = "";
+};
--- time.js
+++ time.js
@@ -28,3 +28,17 @@ const startTime = () => {
 }
 
 startTime();
+
+function timeStamp() {
+  var now = new Date();
+  var date = [now.getDate(), now.getMonth() + 1, now.getFullYear() ];
+  var time = [ now.getHours(), now.getMinutes() ];
+
+  for ( var i = 1; i < 3; i++ ) {
+    if ( time[i] < 10 ) {
+      time[i] = "0" + time[i];
+    }
+  }
+  
+  return time.join(":") + "-" + date.join("/");
+}
